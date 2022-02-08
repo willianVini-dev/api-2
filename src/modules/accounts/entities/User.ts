23e2c96d1bdb8e9137f7eceac8330cb1c9491d95
Willian vinicius <vini-np7@hotmail.com>
2022-02-07 21:20:59 -0300

    migration user / swagger

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,43 @@
+import {v4 as uuidV4} from "uuid"
+import { 
+  Column, 
+  CreateDateColumn, 
+  Entity, 
+  PrimaryColumn } 
+  from "typeorm"
+
+@Entity("users")
+class Users{
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name:string;
+
+  @Column()
+  username:string;
+
+  @Column()
+  email:string;
+
+  @Column()
+  password:string;
+
+  @Column()
+  driver_license:string;
+
+  @Column()
+  idAdmin:boolean;
+
+  @CreateDateColumn()
+  created_at:Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuidV4();
+    }
+  }
+}
+
+export {Users}
