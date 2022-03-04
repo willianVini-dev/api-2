ab33dc7716c3fabaf1eb7e199d2ab919565af185
Willian vinicius <vini-np7@hotmail.com>
2022-03-04 15:33:23 -0300

    create migration rentals

---
+++ src/shared/infra/typeorm/migrations/1646417645890-CreateRentals.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateRentals1646417645890 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+			await queryRunner.createTable( new Table(
+				{
+					name: "rentals",
+					columns:[
+						{name: "id", type:"varchar", isPrimary:true},
+						{name: "car_id", type:"varchar"},
+						{name: "user_id", type:"varchar"},
+						{name: "start_date", type:"timestamp", default: "now()"},
+						{name: "end_date", type:"timestamp"},
+						{name: "expected_return_date", type:"timestamp"},
+						{name: "total", type:"numeric"},
+						{name: "created_at", type:"timestamp",  default: "now()"},
+						{name: "updated_at", type:"timestamp",  default: "now()"}
+					],
+					foreignKeys:[
+						{
+							name: "FKCarRental",
+							referencedTableName: "cars",
+							referencedColumnNames: ["id"],
+							columnNames: ["car_id"],
+						},
+						{
+							name: "FKUserRental",
+							referencedTableName: "users",
+							referencedColumnNames: ["id"],
+							columnNames: ["user_id"]
+						}
+					]
+				}
+			))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+			await queryRunner.dropTable("rentals")
+    }
+
+}
